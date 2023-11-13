8473021e5e2a7babf9e3f0bc5cc6c49429a2253c
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2023-11-13 19:07:47 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next MG版
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          MG版如同Ta的颜色一样奔放<br>https://www.gptacg.com/</br>
+          MG版如同Ta的颜色一样奔放
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
